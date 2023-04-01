729e08cea349d7d26cff3c420072c7712d9288b9
DESKTOP-SDQ8ARP\Dew <natchapon428@gmail.com>
2023-04-01 15:12:11 +0700

    [initial project] initial project spring boot for back end ecommerce web

---
+++ src/test/java/com/skill/kamp/web/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skill.kamp.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
